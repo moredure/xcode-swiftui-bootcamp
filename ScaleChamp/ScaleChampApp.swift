3989745567fa7bb1f4b8a9cabaff94d01bd8a1a5
Mykhailo Faraponov <mfaraponov@cloudlinux.com>
2023-03-09 12:02:07 +0200

    Initial Commit

---
+++ ScaleChamp/ScaleChampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaleChampApp.swift
+//  ScaleChamp
+//
+//  Created by Mykhailo Faraponov on 9.03.23.
+//
+
+import SwiftUI
+
+@main
+struct ScaleChampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
